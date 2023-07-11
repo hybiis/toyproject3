3017427cb98efbaa70350a6c5103bf6461f80172
seonghye0n <seonghye0n@kakao.com>
2023-07-11 16:05:14 +0900

    Initialize Project
    - multi-module 사용 (module-admin : 관리자 페이지 / module-client : 사용자 페이지)
    - 관리자/사용자 페이지 포트 분리 (관리자 : 8081 / 사용자 : 8080)

---
+++ module-client/src/main/java/com/example/moduleclient/ClientApplcation.java
@@ -0,0 +1,11 @@
+package com.example.moduleclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplcation {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplcation.class, args);
+    }
+}
