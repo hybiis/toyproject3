74aca131cd2983e548645ed9f87cc7455ac7bec9
seonghye0n <seonghye0n@kakao.com>
2023-07-12 16:28:00 +0900

    Chore: Spring Security 비활성화
    - Security 구현되면 다시 활성화 필요

--- module-client/src/main/java/com/example/moduleclient/ClientApplcation.java
+++ module-client/src/main/java/com/example/moduleclient/ClientApplcation.java
@@ -2,10 +2,11 @@ package com.example.moduleclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ClientApplcation {
-    public static void main(String[] args) {
-        SpringApplication.run(ClientApplcation.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplcation.class, args);
+	}
 }
