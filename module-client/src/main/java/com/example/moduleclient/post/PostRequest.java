fa7e437fca3a64b88b839785486bfc4c939381df
seonghye0n <seonghye0n@kakao.com>
2023-07-17 16:09:04 +0900

    Feat: RequestDto validation 추가
    - 게시글 작성/수정 시, 제목 or 내용이 null이거나 길이 체크하는 validation 추가

--- module-client/src/main/java/com/example/moduleclient/post/PostRequest.java
+++ module-client/src/main/java/com/example/moduleclient/post/PostRequest.java
@@ -4,6 +4,8 @@ import com.example.moduleclient.constant.Category;
 import com.example.moduleclient.constant.Status;
 import com.example.moduleclient.user.User;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,9 +15,15 @@ public class PostRequest {
 	@NoArgsConstructor
 	@AllArgsConstructor
 	public static class saveDto {
+		@NotBlank
+		@Size(max = 30, message = "제목은 30자를 넘을 수 없습니다.")
 		private String title;
+		@NotBlank
+		@Size(max = 2000, message = "내용은 2000자를 넘을 수 없습니다.")
 		private String content;
+
 		private byte[] thumbnail;
+		@NotBlank
 		private Category category;
 
 		public Post toEntity(User user) {
@@ -34,7 +42,11 @@ public class PostRequest {
 	@NoArgsConstructor
 	@AllArgsConstructor
 	public static class updateDto {
+		@NotBlank
+		@Size(max = 30, message = "제목은 30자를 넘을 수 없습니다.")
 		private String title;
+		@NotBlank
+		@Size(max = 2000, message = "내용은 2000자를 넘을 수 없습니다.")
 		private String content;
 	}
 }
