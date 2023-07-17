8592875ba0e5daef08db4f303a250621865154e3
seonghye0n <seonghye0n@kakao.com>
2023-07-17 16:11:35 +0900

    Feat: RelyRequestDto validation 추가
    - 댓글 작성 시, null 체크 및 길이 체크하는 validation 추가

--- module-client/src/main/java/com/example/moduleclient/reply/ReplyRequest.java
+++ module-client/src/main/java/com/example/moduleclient/reply/ReplyRequest.java
@@ -3,6 +3,8 @@ package com.example.moduleclient.reply;
 import com.example.moduleclient.post.Post;
 import com.example.moduleclient.user.User;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +14,13 @@ public class ReplyRequest {
 	@NoArgsConstructor
 	@AllArgsConstructor
 	public static class saveDto {
+		@NotBlank
+		@Size(max = 50, message = "댓글은 50자를 넘을 수 없습니다.")
 		private String comment;
 		private Long parentId;
+		@NotBlank
 		private int step;
+		@NotBlank
 		private Long postId;
 
 		public Reply toEntity(Post post, User user) {
