d81018e3146c9365218407177a5cc85bd7a2c48f
yunhyeonjin <h00j@naver.com>
2023-07-17 21:56:01 +0900

    fix: 비밀번호 조건 수정

--- module-client/src/main/java/com/example/moduleclient/dto/MemberJoinDTO.java
+++ module-client/src/main/java/com/example/moduleclient/dto/MemberJoinDTO.java
@@ -4,7 +4,10 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 
@@ -13,13 +16,18 @@ import java.time.LocalDate;
 @Data
 public class MemberJoinDTO {
 
+    @NotNull(message = "아이디는 필수 입력 값입니다.")
     private String username;
 
+    @NotNull(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min=8, max=16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
     private String password;
 
+    @NotNull(message = "이메일은 필수 입력 값입니다.")
     @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
 
+    @NotNull(message = "닉네임은 필수 입력 값입니다.")
     private String nickname;
 
     private LocalDate created_at;
