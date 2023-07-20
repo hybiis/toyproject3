4ef85aefe239298139cbb3c441ed21de0ff53019
seonghye0n <seonghye0n@kakao.com>
2023-07-20 23:12:16 +0900

    Chore: Security 적용

--- module-client/src/main/java/com/example/moduleclient/ClientApplcation.java
+++ module-client/src/main/java/com/example/moduleclient/ClientApplcation.java
@@ -2,9 +2,8 @@ package com.example.moduleclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class ClientApplcation {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplcation.class, args);
